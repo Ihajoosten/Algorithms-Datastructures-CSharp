7fe88089ed3fcf537f8e6d235535af772f7195e1
Luc Joosten <lhajoost@avans.nl>
2020-01-20 22:07:03 +0100

    included new alghorithm to calculate saving interests

--- Algorithms_And_Datastructures/Program.cs
+++ Algorithms_And_Datastructures/Program.cs
@@ -10,12 +10,14 @@ namespace Algorithms_And_Datastructures
         {
             //Console.WriteLine("---- Reverse string -------> " + ReverseStringRecursive.ReverseRecursive("pizza"));
 
-            ICollection<int> collection = new List<int>() { 1, 1, 2, 3, 3, 4, 5, 5 };
+            //ICollection<int> collection = new List<int>() { 1, 1, 2, 3, 3, 4, 5, 5 };
 
-            foreach (var item in ReturnDuplicates.ReturnDuplicateValues(collection))
-            {
-                Console.WriteLine("---- Duplicates -------> " + item);
-            }
+            //foreach (var item in ReturnDuplicates.ReturnDuplicateValues(collection))
+            //{
+            //    Console.WriteLine("---- Duplicates -------> " + item);
+            //}
+
+            //Console.WriteLine("New Interests after 2 years: " + SavingInterestsRecursive.CalculateInterests(10, 10, 2));
            
         }
     }
---
+++ Algorithms_And_Datastructures/SavingInterestsRecursive.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms_And_Datastructures
+{
+    public class SavingInterestsRecursive
+    {
+        public static decimal CalculateInterests(decimal begin, int percentage, int years)
+        {
+            if (years == 0) { return begin; }
+            return CalculateInterests(begin * (1 + (percentage / 100)), percentage, years - 1);
+        }
+    }
+}
