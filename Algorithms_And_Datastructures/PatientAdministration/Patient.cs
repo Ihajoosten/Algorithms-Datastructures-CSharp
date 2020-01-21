1fea0f707e33182eb8384dcc098795716191672b
Luc Joosten <lhajoost@avans.nl>
2020-01-21 11:27:16 +0100

    changed patient administration a bit

--- Algorithms_And_Datastructures/PatientAdministration/Patient.cs
+++ Algorithms_And_Datastructures/PatientAdministration/Patient.cs
@@ -1,20 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace Algorithms_And_Datastructures.PatientAdministration
 {
-    public class Patient
+    public class Patient : IComparable<Patient>, IEquatable<Patient>
     {
-        public readonly string Name;
-        public readonly int Age;
-        public readonly double Weight;
+        public string Name { get; }
+        public int Age { get; }
+        public double Weight { get; }
 
         public Patient(string name, int age, double weight)
         {
             this.Name = name;
             this.Age = age;
             this.Weight = weight;
+        }
+
+        public int CompareTo(Patient other)
+        {
+            return Age.CompareTo(other.Age);
+        }
+
+        public bool Equals(Patient other)
+        {
+            return Name.Equals(other.Name) && Age.Equals(other.Age) && Weight.Equals(other.Weight);
+        }
+        public override string ToString()
+        {
+            return "name: " + Name + ", age: " + Age + ", weight: " + Weight;
         }
     }
 }
--- Algorithms_And_Datastructures/PatientAdministration/PatientManagement.cs
+++ Algorithms_And_Datastructures/PatientAdministration/PatientManagement.cs
@@ -7,8 +7,16 @@ namespace Algorithms_And_Datastructures.PatientAdministration
     public class PatientManagement
     {
         // collection of patients
-        private IDictionary<Patient, MedicalRecord> patients = new SortedDictionary<Patient, MedicalRecord>();
+        private IDictionary<Patient, MedicalRecord> patients = new Dictionary<Patient, MedicalRecord>();
         private SortedSet<Patient> sortedPatients = new SortedSet<Patient>();
+
+
+        public void AddPatient(Patient patient, MedicalRecord medicalRecord)
+        {
+            patients.Add(patient, medicalRecord);
+            sortedPatients.Add(patient);
+        }
+
 
         public MedicalRecord FindMedicalRecord(Patient patient)
         {
@@ -17,12 +25,14 @@ namespace Algorithms_And_Datastructures.PatientAdministration
 
         public SortedSet<Patient> FindPatients(int minAge, int maxAge)
         {
-            SortedSet<Patient> patients = new SortedSet<Patient>();
-            foreach (var item in sortedPatients)
-            {
-                if (item.Age >= minAge && item.Age <= maxAge) patients.Add(item);
-            }
-            return patients;
+            //SortedSet<Patient> patients = new SortedSet<Patient>();
+            //foreach (var item in sortedPatients)
+            //{
+            //    if (item.Age >= minAge && item.Age <= maxAge) patients.Add(item);
+            //}
+            //return patients;
+
+            return sortedPatients.GetViewBetween(new Patient("", minAge, 0), new Patient("", maxAge, 0));
         }
     }
 }
